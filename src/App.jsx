8003f9db493c99ec83119bd0272bc6a2e32a9ebc
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-08-28 14:16:53 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1
+     className=' bg-gray-100 text-black font-bold p-4 rounded-lg'
+    >Context api theme switcher</h1>
+    </>
+  )
+}
+
+export default App
